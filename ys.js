617e2226be4081a14d7c8e083104c215076c4dfe
zai1306 <59014879+zai1306@users.noreply.github.com>
2020-07-06 21:46:02 +0500

    ys-org

---
+++ ys.js
@@ -0,0 +1,21 @@
+// var swiper = new Swiper('.swiper-container', {
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'progressbar',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+
+
+  window.addEventListener('scroll', function(){
+      var header = document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollTo > 0)
+  })
+
+
+
+//  “Pakistan is proud of her youth, particularly the students who have always been in the forefront in the hour of trial and need. You are the nation’s leaders of tomorrow and you must fully equip yourself by discipline, education and training for the arduous task lying ahead of you. You should realise the magnitude of your responsibility and be ready to bear it.”
+
